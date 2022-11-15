bf54bdc3ed57cf49dabd820b32ea35e697a1ef48
LeonardoADS08 <leonardoads08@gmail.com>
2022-11-14 22:58:26 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using EmailSender.Core;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Net.Mail;
+using System.Net;
+using Microsoft.Extensions.Configuration;
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((hostContext, services) =>
+    {
+        var smtp = new SmtpClient
+        {
+            Host = "smtp.gmail.com",
+            Port = 587,
+            EnableSsl = true,
+            DeliveryMethod = SmtpDeliveryMethod.Network,
+            UseDefaultCredentials = false
+        };
+
+        var configurations = new Configuration()
+        services.AddScoped<SmtpClient>(builder => smtp);
+        services.AddScoped<IAppConfigurations, AppConfigurations>();
+        services.AddScoped<IEmailSenderService, EmailSenderService>();
+
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
---
+++ Worker.cs
@@ -0,0 +1,22 @@
+﻿using EmailSender.Core;
+using Microsoft.Extensions.Hosting;
+using PanoramicData.ConsoleExtensions;
+
+public class Worker : BackgroundService
+{
+    private readonly IEmailSenderService _emailSenderService;
+
+    public Worker(IEmailSenderService emailSenderService)
+    {
+        _emailSenderService = emailSenderService;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        Console.Write("Password: ");
+        var password = ConsolePlus.ReadPassword();
+        Console.WriteLine();
+
+        var result = await _emailSenderService.SendEmailBatchAsync(password);
+    }
+}
